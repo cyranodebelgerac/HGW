5b5c470e77b30351e89ce0a7c2d665f4bf03a4ab
cyranodebelgerac <35063310+cyranodebelgerac@users.noreply.github.com>
2018-11-22 11:41:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+import random
+
+###########################################################
+# set basic parameters for a servant. 
 class Servant:
     def __init__(self, name, servant_class, hp, atk, rarity):
         self.name = name
@@ -9,35 +13,39 @@ class Servant:
         self.hp = self.hp - saba_atk * modifier
 
 ###########################################################
-        
+# list of servants
+saba = [
+        Servant("Artoria Pendragon", "Saber", 10000, 1000, "5"), 
+        Servant("EMIYA", "Archer", 8500, 900, "4"),
+        Servant("Cu Chulainn", "Lancer", 9000, 950, "3"),
+        Servant("Medusa", "Rider", 10000, 1000, "3"),
+        Servant("Sasaki Kojirou", "Assassin", 9000, 1200, "1"),
+        Servant("Medea", "Caster", 8500, 900, "3"),
+        Servant("Heracles", "Berserker", 10000, 1000, "4")
+        ]
 
-saba001 = Servant("Artoria Pendragon", "Saber", 10000, 1000, "5")
-saba002 = Servant("EMIYA", "Archer", 8500, 900, "4")
-saba003 = Servant("Cu Chulainn", "Lancer", 8500, 900, "4")
-saba004 = Servant("Medusa", "Rider", 10000, 1000, "5")
-saba005 = Servant("Sasaki Kojirou", "Assassin", 9000, 1200, "4")
-saba006 = Servant("Medea", "Caster", 8500, 900, "4")
-saba007 = Servant("Heracles", "Berserker", 10000, 1000, "5")
-
+# generates a string with all servants' names separated by a comma. 
+saba_namelist = ""
+for x in range(len(saba)):
+	saba_namelist = saba_namelist + saba[x].name+ ", "
 
 ###########################################################
-
-
+# player inputs a servant name, determines that it's valid
 def servant_selection():
-    choice = ""
-    while choice not in (saba001.name,saba002.name,saba003.name,saba004.name,saba005.name,saba006.name,saba007.name):
-        choice = input("Choose a servant: \n")
-    for x in (saba001,saba002,saba003,saba004,saba005,saba006,saba007):
-        if choice == x.name:
-            return x
+    choice = "   "
+    while choice + "," not in saba_namelist.upper():
+        choice = input("Choose a servant: \n").upper()
+    return choice
 
-def enemy_selection():
-    choice = ""
-    while choice not in (saba001.name,saba002.name,saba003.name,saba004.name,saba005.name,saba006.name,saba007.name):
-        choice = input("Choose an enemy: \n")
-    for x in (saba001,saba002,saba003,saba004,saba005,saba006,saba007):
-        if choice == x.name:
-            return x
+# takes in servant name (verified by servant_selection) and returns an actual servant
+def set_servant(name):
+    for x in range(len(saba)):
+        if name == saba[x].name.upper():
+            return saba[x]
+        
+# returns a random servant
+def rand_servant():
+    return saba[random.randint(1,len(saba) - 1)]
 
 def class_adv(saba_class, enemy_class):
     if saba_class == "Saber":
@@ -45,52 +53,44 @@ def class_adv(saba_class, enemy_class):
             return 2
         if enemy_class == "Berserker":
             return 1.5
-
     if saba_class == "Archer":
         if enemy_class == "Lancer":
             return 2
         if enemy_class == "Berserker":
             return 1.5
         return 1
-
     if saba_class == "Lancer":
         if enemy_class == "Saber":
             return 2
         if enemy_class == "Berserker":
             return 1.5
-
     if saba_class == "Rider":
         if enemy_class == "Assassin":
             return 2
         if enemy_class == "Berserker":
             return 1.5
-
     if saba_class == "Assassin":
         if enemy_class == "Caster":
             return 2
         if enemy_class == "Berserker":
             return 1.5
         return 1
-
     if saba_class == "Caster":
         if enemy_class == "Rider":
             return 2
         if enemy_class == "Berserker":
             return 1.5
-
     if saba_class == "Berserker":
         return 2
-    
     return 1
-
             
 ###########################################################
-
-
 print("Welcome to the Holy Grail War!")
 
-player = servant_selection()
-enemy = enemy_selection()
+choice = "  "
+player_choice = servant_selection()
+player = set_servant(player_choice)
+enemy = rand_servant()
 
 modifier = class_adv(player.servant_class, enemy.servant_class)
 enemy_modifier = class_adv(enemy.servant_class, player.servant_class)
