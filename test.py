b80dc461fa1c15aac7932ba39b873dad1a723b4f
cyranodebelgerac <35063310+cyranodebelgerac@users.noreply.github.com>
2018-11-22 16:45:22 +0900

    Update test.py

--- test.py
+++ test.py
@@ -10,30 +10,35 @@ class Servant:
         self.atk = atk
         self.rarity = rarity
     def take_dmg(self, saba_atk, modifier):
-        self.hp = self.hp - saba_atk * modifier
+        self.hp = int(self.hp - saba_atk * modifier)
+    def heal(self, amount):
+        self.hp =int(self.hp + amount)
 
 ###########################################################
 # list of servants
 saba = [
         Servant("Artoria Pendragon", "Saber", 10000, 1000, "5"), 
         Servant("EMIYA", "Archer", 8500, 900, "4"),
-        Servant("Cu Chulainn", "Lancer", 9000, 950, "3"),
+        Servant("Cu Chulainn", "Lancer", 9000, 1050, "3"),
         Servant("Medusa", "Rider", 10000, 1000, "3"),
         Servant("Sasaki Kojirou", "Assassin", 9000, 1200, "1"),
         Servant("Medea", "Caster", 8500, 900, "3"),
-        Servant("Heracles", "Berserker", 10000, 1000, "4")
+        Servant("Heracles", "Berserker", 10000, 1000, "4"),
+        Servant("Jeanne D'arc", "Ruler", 12000, 600, "5"),
+        Servant("Angra Mainyu", "Avenger", 6000, 600, "3"),
+        Servant("BB", "Moon Cancer", 10000, 1000, "4")
         ]
 
-# generates a string with all servants' names separated by a comma. 
-saba_namelist = ""
+# generates a string with all servants' names separated by a comma. Used in servant_select
+saba_list = ""
 for x in range(len(saba)):
-	saba_namelist = saba_namelist + saba[x].name+ ", "
+    saba_list = saba_list + saba[x].name + ", "
 
 ###########################################################
-# player inputs a servant name, determines that it's valid
-def servant_selection():
+# player inputs a servant name, determines that it's valid. Used at the beginning of the war.
+def servant_select():
     choice = "   "
-    while choice + "," not in saba_namelist.upper():
+    while choice + "," not in saba_list.upper():
         choice = input("Choose a servant: \n").upper()
     return choice
 
@@ -45,74 +50,77 @@ def set_servant(name):
         
 # returns a random servant
 def rand_servant():
-    return saba[random.randint(1,len(saba) - 1)]
+    return saba[random.randint(0,len(saba) - 1)]
 
+# determines class advantage dmg. these next two functions go together.
+# in case of nero caster's skill, do modifier = modifier/modifier i guess? 
+def reg_adv_modifier(enemy_class, adv, disadv, berserker):
+    if enemy_class in adv:
+        return 2
+    if enemy_class in disadv:
+        return 0.5
+    if berserker:
+        return 1.5
+    
 def class_adv(saba_class, enemy_class):
     if saba_class == "Saber":
-        if enemy_class == "Archer":
-            return 2
-        if enemy_class == "Berserker":
-            return 1.5
+        return reg_adv_modifier(enemy_class, "Archer", "Lancer", 1)
     if saba_class == "Archer":
-        if enemy_class == "Lancer":
-            return 2
-        if enemy_class == "Berserker":
-            return 1.5
-        return 1
+        return reg_adv_modifier(enemy_class, "Lancer", "Saber", 1)
     if saba_class == "Lancer":
-        if enemy_class == "Saber":
-            return 2
-        if enemy_class == "Berserker":
-            return 1.5
+        return reg_adv_modifier(enemy_class, "Saber", "Archer", 1)
     if saba_class == "Rider":
-        if enemy_class == "Assassin":
-            return 2
-        if enemy_class == "Berserker":
-            return 1.5
+        return reg_adv_modifier(enemy_class, "Assassin", "Caster", 1)
     if saba_class == "Assassin":
-        if enemy_class == "Caster":
-            return 2
-        if enemy_class == "Berserker":
-            return 1.5
-        return 1
+        return reg_adv_modifier(enemy_class, "Caster", "Rider", 1)
     if saba_class == "Caster":
-        if enemy_class == "Rider":
-            return 2
-        if enemy_class == "Berserker":
-            return 1.5
+        return reg_adv_modifier(enemy_class, "Rider", "Assassin", 1)
     if saba_class == "Berserker":
         return 2
+    if saba_class == "Ruler":
+        return reg_adv_modifier(enemy_class, "Avenger", "Moon CancerSaberArcherLancerRiderAssassinCaster", 1)
+    if saba_class == "Avenger":
+        return reg_adv_modifier(enemy_class, "Moon Cancer", "Ruler", 1)
+    if saba_class == "Moon Cancer":
+        return reg_adv_modifier(enemy_class, "Ruler", "Avenger", 1)
     return 1
+
+def combat(player, enemy):
+    modifier = class_adv(player.servant_class, enemy.servant_class)
+    player.take_dmg(enemy.atk, modifier)
+    print("{} attacks! {} takes {} damage!".format(enemy.name,player.name, int(enemy.atk * modifier)))
+
+def victory(servant):
+    if servant.hp <= 0:
+        return 1
             
 ###########################################################
 print("Welcome to the Holy Grail War!")
 
 choice = "  "
-player_choice = servant_selection()
-player = set_servant(player_choice)
+player = set_servant(servant_select())
 enemy = rand_servant()
 
-modifier = class_adv(player.servant_class, enemy.servant_class)
-enemy_modifier = class_adv(enemy.servant_class, player.servant_class)
+turn = 1
 
-while player.hp > 0 or enemy.hp > 0:
-    print("*"*10)
+while 1:
+    print("*"*10 + "Turn {}".format(turn) + "*"*10)
     print("{} has {} HP left!".format(player.name, player.hp))
-    print("*"*10)
     print("{} has {} HP left!".format(enemy.name, enemy.hp))
     print("*"*10)
     
-    player.take_dmg(enemy.atk, modifier)
-    print("{} attacks! {} takes {} damage!".format(enemy.name,player.name, enemy.atk * modifier))
-    if player.hp <= 0:
-        input("---------------------")
-        print("{} has won!".format(enemy.name))
+    combat(player, enemy)
+    if victory(player):
+        winner = enemy
         break
+    
+    combat(enemy, player)
+    if victory(enemy):
+        winner = player
+        break
+    else:
+        turn += 1
+        input("")
         
-    enemy.take_dmg(player.atk, enemy_modifier)
-    print("{} attacks! {} takes {} damage!".format(player.name,enemy.name, player.atk * enemy_modifier))
-    if enemy.hp <= 0:
-        input("---------------------")
-        print("{} has won!".format(player.name))
-        break
-    input("---------------------")
+print("---------------------")
+print("{} has won!".format(winner.name))
